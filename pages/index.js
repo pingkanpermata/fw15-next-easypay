2c689225b866e568969b6a343d7286077d6b0164
pingkanpermata <yuko.IT.programming@gmail.com>
2023-06-12 12:32:59 +0700

    design mockup

--- pages/index.js
+++ pages/index.js
@@ -8,34 +8,34 @@ export default function Home() {
     <>
       <nav className="flex bg-[#f5f5f5] px-20 py-5 items-center">
         <div className="grow">
-          <div className="text-3xl text-[#2C74B3] font-bold">Trust-Pay</div>
+          <img className="w-[25%]" src="/logo-easypay.png" alt="" />
         </div>
 
         <div className="flex gap-3">
           <div>
-            <Link href='/login' className="rounded-xl py-2 px-8 w-[110px] font-bold border-2 border-[#2C74B3] text-[#2C74B3] bg-white">Login</Link>
+            <Link href='/login' className="rounded-xl py-2 px-8 w-[110px] font-bold border-2 border-[#dbc999] hover:bg-[#dac387] text-[#3b3524] hover:text-white bg-white">Login</Link>
           </div>
           <div>
-            <Link href='/register' className="rounded-xl py-2 px-6  w-[110px] font-bold border-2 border-[#2C74B3] text-white bg-[#2C74B3]">Sign Up</Link>
+            <Link href='/register' className="rounded-xl py-2 px-6  w-[110px] font-bold border-2 border-[#dbc999] text-white bg-[#d3ba7a] hover:bg-[#d7c28a]">Sign Up</Link>
           </div>
         </div>
       </nav>
 
       <main>
-        <section className="flex px-20 gap-20 items-center justify-center gap-20 bg-[#f5f5f5] pb-8">
+        <section className="flex px-20 gap-20 items-center justify-center bg-[#f5f5f5] pb-8">
           <div>
-            <img src="/Group 49.svg" alt="" />
+            <img src="/Group 9.svg" alt="" />
           </div>
 
           <div className="flex flex-col gap-5">
             <div className="text-6xl font-bold w-[500px] leading-relaxed">
-              Awesome App For Saving <span className="text-[#2C74B3]">Time.</span>
+              Awesome App For Saving <span className="text-[#857752]">Time.</span>
             </div>
 
             <div>
               <div className="w-[400px] mb-7">We bring you a mobile app for banking problems that oftenly wasting much of your times.</div>
               <div>
-                <button className="rounded-xl py-3 w-[150px] border-2 border-[#2C74B3] text-white bg-[#2C74B3]">Try It Free</button>
+                <button className="rounded-xl py-3 w-[150px] border-2 border-[#d3ba7a] text-white bg-[#d3ba7a] hover:bg-[#d7c28a]">Try It Free</button>
               </div>
             </div>
 
@@ -78,7 +78,7 @@ export default function Home() {
         <section className="bg-[#f5f5f5] px-20 py-20">
           <div className="text-center mb-10">
             <div className="text-6xl font-bold mb-10">
-              <span className="text-[#2C74B3]">About</span> the Application.
+              <span className="text-[#857752]">About</span> the Application.
             </div>
             <p>
               We have some great features from the application and it’s totally <br /> free to use by all users around the world.
@@ -110,7 +110,7 @@ export default function Home() {
                   <img src="/downloadIcon.svg" />
                 </div>
                 <div className="font-bold mb-5 text-xl">Easy Download</div>
-                <div>Zwallet is 100% totally free to use it’s now available on Google Play Store and App Store.</div>
+                <div>EasyPay is 100% totally free to use it’s now available on Google Play Store and App Store.</div>
               </div>
             </div>
           </div>
@@ -118,28 +118,28 @@ export default function Home() {
 
         <section className="flex px-20 py-10 gap-20 items-center justify-center bg-[#E5E5E5]">
           <div>
-            <img src="/Group 54.svg" />
+            <img src="/Group 8.svg" />
           </div>
 
           <div className="flex flex-col gap-5">
             <div className="text-6xl font-bold w-[500px] leading-relaxed">
-              All The <span className="text-[#2C74B3]">Great</span> <br />
+              All The <span className="text-[#857752]">Great</span> <br />
               Trust-Pay Features.
             </div>
 
             <div className="card w-100 bg-white">
               <div className="p-5 flex flex-col">
                 <div className="font-bold">
-                  <span className="text-[#2C74B3]">1.</span>Small Fee
+                  <span className="text-[#857752]">1.</span>Small Fee
                 </div>
-                <div>We only charge 5% of every success transaction done in Trust-Pay app.</div>
+                <div>We only charge 5% of every success transaction done in EasyPay app.</div>
               </div>
             </div>
 
             <div className="card w-100 bg-white">
               <div className="p-5 flex flex-col">
                 <div className="font-bold">
-                  <span className="text-[#2C74B3]">2.</span>Data Secured
+                  <span className="text-[#857752]">2.</span>Data Secured
                 </div>
                 <div>All your data is secured properly in our system and it’s encrypted.</div>
               </div>
@@ -148,9 +148,9 @@ export default function Home() {
             <div className="card w-100 bg-white">
               <div className="p-5 flex flex-col">
                 <div className="font-bold">
-                  <span className="text-[#2C74B3]">3.</span>User Friendly
+                  <span className="text-[#857752]">3.</span>User Friendly
                 </div>
-                <div>Trust-Pay come up with modern and sleek design and not complicated.</div>
+                <div>EasyPay come up with modern and sleek design and not complicated.</div>
               </div>
             </div>
           </div>
@@ -159,7 +159,7 @@ export default function Home() {
         <section className="px-20 pt-24 pb-20 bg-[#f5f5f5]">
           <div className="text-center mb-10">
             <div className="text-6xl font-bold mb-12">
-              What Users are <span className="text-[#2C74B3]">Saying.</span>
+              What Users are <span className="text-[#857752]">Saying.</span>
             </div>
             <div>We have some great features from the application and it’s totally free <br/> to use by all users around the world.</div>
           </div>
@@ -192,14 +192,14 @@ export default function Home() {
       </main>
 
       <footer className="
-      bg-[#2C74B3] px-20 py-14 text-white">
-        <div className="text-2xl font-bold mb-8">Trust-Pay</div>
+      bg-[#dac387] px-20 py-14 text-white">
+        <img className="w-[15%]" src="/logo-easypay.png" alt="" />
         <div className="mb-8">Simplify financial needs and saving <br/> much time in banking needs with <br/> one single app.</div>
         <hr/>
         <div className="flex mt-5 gap-10">
-          <div className="grow">2022 Trust-Pay. All right reserved.</div>
+          <div className="grow">2023 EasyPay. All right reserved.</div>
           <div>+62 5637 8882 9901</div>
-          <div>contact@TrustPay.com</div>
+          <div>contact@EasyPay.com</div>
         </div>
       </footer>
     </>
