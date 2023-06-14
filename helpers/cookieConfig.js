85009549f931ba28f0b14efba7ba926b29ea5b49
pingkanpermata <yuko.IT.programming@gmail.com>
2023-06-14 10:05:44 +0700

    .

--- helpers/cookieConfig.js
+++ helpers/cookieConfig.js
@@ -7,14 +7,3 @@ const cookieConfig = {
 };
 
 export default cookieConfig;
-
-// const cookieConfig = {
-//     cookieName: "ZIpay",
-//     password: "N>x3Vq&9?)kPzp]GAg>y=$s3^r$GcPFQ",
-//     // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-//     cookieOptions: {
-//         secure: process.env.NODE_ENV === "production",
-//     },
-// };
-
-// export default cookieConfig;
